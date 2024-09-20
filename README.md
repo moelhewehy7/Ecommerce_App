65b1c4fa388cbc9e3741eaec180f403fa7720a5f
mohamedelhewehy <m.osama_el7ewehy@yahoo.com>
2024-09-20 20:48:29 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # Ecommerce_App
+=======
+# ecommerce_app
+
+A new Flutter project.
+>>>>>>> db40dfd (first commit)
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle styleNunitoSansRegular12 = TextStyle(
+    fontFamily: "Nunito Sans",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff000000),
+    height: 36 / 12,
+  );
+  static const TextStyle styleRalewayBold17 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 17,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 21 / 17,
+  );
+  static const TextStyle styleRalewayBold14 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 21 / 17,
+  );
+  static const TextStyle styleNunitoSansSemiBold14 = TextStyle(
+    fontFamily: "Nunito Sans",
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff000000),
+    height: 16 / 14,
+  );
+  static const TextStyle styleRalewayMedium13 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff202020),
+    height: 17 / 13,
+  );
+
+  static const TextStyle styleRalewayeBold14 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 18 / 14,
+  );
+  static const TextStyle styleRalewayeBold13 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 13,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 18 / 14,
+  );
+  static const TextStyle styleRalewayeBold21 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 21,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 18 / 14,
+  );
+}
