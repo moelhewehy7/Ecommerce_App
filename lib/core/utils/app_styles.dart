4de472573a293057d31db177f32bafe4b8d5ff20
mohamedelhewehy <m.osama_el7ewehy@yahoo.com>
2024-09-20 21:21:42 +0300

    adding font AppStyles

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,3 +1,5 @@
+// import 'package:flutter/material.dart';
+
 import 'package:flutter/material.dart';
 
 class AppStyles {
@@ -8,12 +10,54 @@ class AppStyles {
     color: Color(0xff000000),
     height: 36 / 12,
   );
+  static const TextStyle styleNunitoSansLight19 = TextStyle(
+    fontFamily: "Nunito Sans",
+    fontSize: 19,
+    fontWeight: FontWeight.w300,
+    color: Color(0xff202020),
+    height: 59 / 19,
+  );
+  static const TextStyle styleNunitoSansLight22 = TextStyle(
+    fontFamily: "Nunito Sans",
+    fontSize: 22,
+    fontWeight: FontWeight.w300,
+    color: Color(0xfff3f3f3),
+    height: 31 / 22,
+  );
+  static const TextStyle styleNunitoSansLight15 = TextStyle(
+    fontFamily: "Nunito Sans",
+    fontSize: 15,
+    fontWeight: FontWeight.w300,
+    color: Color(0xfff3f3f3),
+    height: 31 / 22,
+  );
   static const TextStyle styleRalewayBold17 = TextStyle(
     fontFamily: "Raleway",
     fontSize: 17,
     fontWeight: FontWeight.w700,
     color: Color(0xff202020),
     height: 21 / 17,
+  );
+  static const TextStyle styleRalewayBold58 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 50,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 108 / 50,
+  );
+  static const TextStyle stylePoppinsMedium20 = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 13.834808349609375,
+    fontWeight: FontWeight.w500,
+    color: Color(0xffd2d2d2),
+    height: 19 / 13.834808349609375,
+  );
+  static const TextStyle stylePoppinsMedium14 = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Color(0xffd2d2d2),
+    height: 19 / 14,
   );
   static const TextStyle styleRalewayBold14 = TextStyle(
     fontFamily: "Raleway",
@@ -56,6 +100,19 @@ class AppStyles {
     fontSize: 21,
     fontWeight: FontWeight.w700,
     color: Color(0xff202020),
-    height: 18 / 14,
+  );
+  static const TextStyle styleRalewayeMedium16 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff010100),
+    height: 20 / 16,
+  );
+  static const TextStyle styleRalewayBold28 = TextStyle(
+    fontFamily: "Raleway",
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff202020),
+    height: 36 / 28,
   );
 }
