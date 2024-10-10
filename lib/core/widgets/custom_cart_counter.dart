c2b3340f0d39fef9fcd45015e60122b01444ad9d
mohamedelhewehy <m.osama_el7ewehy@yahoo.com>
2024-10-10 19:37:17 +0300

    replace stack counter with Badge.count

--- lib/core/widgets/custom_cart_counter.dart
+++ lib/core/widgets/custom_cart_counter.dart
@@ -12,34 +12,20 @@ class CustomCartCounter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        IconButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const MyCart()));
-            },
-            icon: Icon(
-              Iconsax.shopping_bag_bold,
-              color: color,
-            )),
-        Positioned(
-          top: 0,
-          right: 2,
-          child: Container(
-            height: 20,
-            width: 20,
-            decoration: ShapeDecoration(
-                color: Theme.of(context).colorScheme.onSecondaryFixedVariant,
-                shape: const CircleBorder(side: BorderSide.none)),
-            child: const Center(
-                child: Text(
-              "2",
-              style: TextStyle(fontSize: 12, color: Colors.white),
-            )),
-          ),
-        )
-      ],
+    return IconButton(
+      icon: Badge.count(
+        textColor: Colors.white,
+        backgroundColor: Theme.of(context).colorScheme.onSecondaryFixedVariant,
+        count: 99,
+        child: Icon(
+          Iconsax.shopping_bag_bold,
+          color: color,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const MyCart()));
+      },
     );
   }
 }
